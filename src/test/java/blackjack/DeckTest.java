e620edf2e03bd785c12d4b5de033c1937612e7a5
Haimanot Tekie <hftek@nets.eu>
2018-12-28 02:12:34 +0100

    deck class - with constructor that creates deck based on comma separated string

---
+++ src/test/java/blackjack/DeckTest.java
@@ -0,0 +1,15 @@
+package blackjack;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DeckTest {
+
+    @Test
+    public void create_deck_from_comma_separated_string(){
+        Deck deck = new Deck("CA, D4, H7, SJ");
+        assertEquals("CA, D4, H7, SJ", deck.printCardIds());
+    }
+
+}
