38788abdd5d3064c182bf53db489f92afc7df9fb
Haimanot1989 <haimif@yahoo.com>
2019-01-02 01:40:48 +0100

    read file path from args(program argument in idea)

--- src/main/java/blackjack/Application.java
+++ src/main/java/blackjack/Application.java
@@ -1,19 +1,15 @@
 package blackjack;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
 public class Application {
-	@Value("${input.file}")
-	private static String inputFile;
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-        System.out.println(inputFile);
-        Game game = new Game(inputFile);
+        Game game = new Game(args[0]);
         game.simulateGame();
     }
 }
