42af3b04abb04e53382a8a18ad3681955c1b39e6
Haimanot1989 <haimif@yahoo.com>
2019-01-08 01:59:01 +0100

    model player and add som test

--- src/main/java/blackjack/Player.java
+++ src/main/java/blackjack/Player.java
@@ -2,13 +2,46 @@ package blackjack;
 
 public class Player {
     private String name;
+    private Hand hand;
 
-    public Player(String name) {
+    Player(String name) {
         this.name = name;
+        this.hand = new Hand();
     }
 
-    public String getName(){
-        return this.name;
+    String getName(){
+        return name;
+    }
+
+    void drawCard(Deck deck, int limitScore){
+        while (hand.score() <= limitScore && !deck.isEmpty()){
+            hand.addCard(deck.dealCard());
+        }
+    }
+    void receiveCard(Card card){
+        hand.addCard(card);
+    }
+
+    boolean hasBlackjack(){
+        return hand.isInitiallyBlackjack();
+    }
+
+    boolean hasDoubleAce(){
+        return hand.isInitiallyDoubleAce();
+    }
+
+    boolean hasBustedHand(){
+        return hand.busted();
+    }
+
+    int score(){
+        return hand.score();
+    }
+
+
+    @Override
+    public String toString(){
+        return name + ": " + hand;
     }
 
 }
